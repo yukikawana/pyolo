8047682491020bf6fed91dcc5538e7345a1027cd
Tohge Norihito <dimension8300@gmail.com>
2017-01-09 15:00:24 +0900

    Captured images are shown even when no object is detected.

--- demo.py
+++ demo.py
@@ -13,22 +13,20 @@ while(cap0.isOpened()):
     ret0, frame0 = cap0.read()
     result = pyolo.predict(frame0)
 
-    if not len(result) > 0:  # if not object is detected continue
-        continue
+    if len(result) > 0:  # if object is detected
+        for id in range(len(result)):
+            result_by_id = result[id]
+            if min(result_by_id) < 0:
+                continue
 
-    for id in range(len(result)):
-        result_by_id = result[id]
-        if min(result_by_id) < 0:
-            continue
-
-        left = result_by_id[2]
-        top = result_by_id[4]
-        right = result_by_id[3]
-        bottom = result_by_id[5]
-        cv.rectangle(frame0, (left, top), (right, bottom), (0, 0, 255), 2)
-        cv.imshow('frame0', frame0)
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+            left = result_by_id[2]
+            top = result_by_id[4]
+            right = result_by_id[3]
+            bottom = result_by_id[5]
+            cv.rectangle(frame0, (left, top), (right, bottom), (0, 0, 255), 2)
+    cv.imshow('frame0', frame0)
+    if cv.waitKey(10) & 0xFF == ord('q'):
+        break
 
 cap0.release()
 cv.destroyAllWindows()
